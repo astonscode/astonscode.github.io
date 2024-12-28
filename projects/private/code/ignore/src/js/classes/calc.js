07e41031228c2230a13c91ebf757fb6b85fd773b
aston <70747479+astonscode@users.noreply.github.com>
2024-12-28 01:17:02 -0500

    awaiting input
    Down for maintenance.
    
    Removed all files/projects.

--- projects/private/code/ignore/src/js/classes/calc.js
+++
@@ -1,6 +0,0 @@
-function quadratics(a, b, c) {
-	let plus = (b * -1 + Math.sqrt(Math.pow(b, 2) - -4 * a * c)) / (2 * a)
-	let minus = (b * -1 - Math.sqrt(Math.pow(b, 2) - -4 * a * c)) / (2 * a)
-
-	console.log("%cCALC", "background: #047eff;  padding: 4px; margin: 8px; color: white", `± [${plus.toFixed(2)}, ${minus.toFixed(2)}]`)
-}
